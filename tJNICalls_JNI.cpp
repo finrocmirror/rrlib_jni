fae9be0eaec41dd992ed053938b9aaf132651abe
Max Reichardt <reichardt@informatik.uni-kl.de>
2010-04-13 17:29:43 +0000

    initial commmit

---
+++ tJNICalls_JNI.cpp
@@ -0,0 +1,143 @@
+//This is a generated JNI wrapper file
+
+/**
+ * You received this file as part of an advanced experimental
+ * robotics framework prototype ('finroc')
+ *
+ * Copyright (C) 2007-2010 Max Reichardt,
+ *   Robotics Research Lab, University of Kaiserslautern
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include "finroc_core_utils_jni/tJNIConvert.h"
+
+namespace jniwrap
+{
+} // namespace jniwrap
+
+extern "C"
+{
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_deleteJNIWrappable(JNIEnv* env, jclass class_, jlong pointer)
+  {
+    finroc::util::tJNIWrappable* obj = (finroc::util::tJNIWrappable*)pointer;
+    obj->SetJavaWrapper(NULL, false); // avoids that Java "destructor" is invoked (again)
+    delete obj;
+  }
+  JNIEXPORT jlong JNICALL Java_org_finroc_jc_jni_JNICalls_getBufferPointer(JNIEnv* env, jclass class_, jobject buf)
+  {
+    return (jlong)env->GetDirectBufferAddress(buf);
+  }
+  JNIEXPORT jbyte JNICALL Java_org_finroc_jc_jni_JNICalls_getByte(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jbyte*)ptr);
+  }
+  JNIEXPORT jobject JNICALL Java_org_finroc_jc_jni_JNICalls_getCByteBuffer(JNIEnv* env, jclass class_, jlong ptr, jint size)
+  {
+    jobject result;
+    result = env->NewDirectByteBuffer((void*)ptr, size);
+    return result;
+  }
+  JNIEXPORT jdouble JNICALL Java_org_finroc_jc_jni_JNICalls_getDouble(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jdouble*)ptr);
+  }
+  JNIEXPORT jfloat JNICALL Java_org_finroc_jc_jni_JNICalls_getFloat(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jfloat*)ptr);
+  }
+  JNIEXPORT jint JNICALL Java_org_finroc_jc_jni_JNICalls_getInt(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jint*)ptr);
+  }
+  JNIEXPORT jlong JNICALL Java_org_finroc_jc_jni_JNICalls_getJavaVM(JNIEnv* env, jclass class_)
+  {
+    JavaVM* jvm = finroc::util::tJNIHelper::GetJavaVM();
+    if (jvm != NULL)
+    {
+      return (jlong)jvm;
+    }
+    env->GetJavaVM(&jvm);
+    assert(jvm != NULL && "Error initializing JavaVM pointer");
+    finroc::util::tJNIHelper::SetJavaVM(jvm);
+    return (jlong)jvm;
+  }
+  JNIEXPORT jlong JNICALL Java_org_finroc_jc_jni_JNICalls_getLong(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jlong*)ptr);
+  }
+  JNIEXPORT jlong JNICALL Java_org_finroc_jc_jni_JNICalls_getPointer(JNIEnv* env, jclass class_, jlong address, jint index)
+  {
+    void** array = (void**)address;
+    return (jlong)array[index];
+  }
+  JNIEXPORT jshort JNICALL Java_org_finroc_jc_jni_JNICalls_getShort(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return *((jshort*)ptr);
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_memcpy(JNIEnv* env, jclass class_, jlong dest, jlong src, jint length)
+  {
+    memcpy((void*)src, (void*)dest, (int)length);
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setByte(JNIEnv* env, jclass class_, jlong ptr, jbyte val)
+  {
+    *((jbyte*)ptr) = val;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setDouble(JNIEnv* env, jclass class_, jlong ptr, jdouble val)
+  {
+    *((jdouble*)ptr) = val;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setFloat(JNIEnv* env, jclass class_, jlong ptr, jfloat val)
+  {
+    *((jfloat*)ptr) = val;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setInt(JNIEnv* env, jclass class_, jlong ptr, jint val)
+  {
+    *((jint*)ptr) = val;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setJavaObject(JNIEnv* env, jclass class_, jlong pointer, jobject object, jboolean java_responsible)
+  {
+    finroc::util::tJNIWrappable* obj = (finroc::util::tJNIWrappable*)pointer;
+    obj->SetJavaWrapper(object, !java_responsible);
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setLong(JNIEnv* env, jclass class_, jlong ptr, jlong val)
+  {
+    *((jlong*)ptr) = val;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setPointer(JNIEnv* env, jclass class_, jlong arraypointer, jint index, jlong pointer)
+  {
+    void** array = (void**)arraypointer;
+    array[index] = (void*)pointer;
+  }
+  JNIEXPORT void JNICALL Java_org_finroc_jc_jni_JNICalls_setShort(JNIEnv* env, jclass class_, jlong ptr, jshort val)
+  {
+    *((jshort*)ptr) = val;
+  }
+  JNIEXPORT jint JNICALL Java_org_finroc_jc_jni_JNICalls_sizeOfPointer(JNIEnv* env, jclass class_)
+  {
+    return sizeof(void*);
+  }
+  JNIEXPORT jint JNICALL Java_org_finroc_jc_jni_JNICalls_strlen(JNIEnv* env, jclass class_, jlong ptr)
+  {
+    return strlen((char*)ptr);
+  }
+  JNIEXPORT jstring JNICALL Java_org_finroc_jc_jni_JNICalls_toString(JNIEnv* env, jclass class_, jlong pointer)
+  {
+    jstring result;
+    result = env->NewStringUTF((char*)pointer);
+    return result;
+  }
+
+} // extern C
+
---
+++ tJNIConvert.h
@@ -0,0 +1,232 @@
+/**
+ * You received this file as part of an advanced experimental
+ * robotics framework prototype ('finroc')
+ *
+ * Copyright (C) 2007-2010 Max Reichardt,
+ *   Robotics Research Lab, University of Kaiserslautern
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef RRLIB__FINROC_CORE_UTILS_JNI__TJNICONVERT_H
+#define RRLIB__FINROC_CORE_UTILS_JNI__TJNICONVERT_H
+
+#include "finroc_core_utils/tJCBase.h"
+
+#ifdef __JC_CREATE_JNI_WRAPPERS__
+
+namespace finroc
+{
+namespace util
+{
+namespace jni
+{
+
+///////////////////////////////////////////////////////////////////////////////
+// conversion methods C++ to Java
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+jbyte ToJbyte(T v)
+{
+  return static_cast<jbyte>(v);
+}
+
+template <typename T>
+jshort ToJshort(T v)
+{
+  return static_cast<jshort>(v);
+}
+
+template <typename T>
+jint ToJint(T v)
+{
+  return static_cast<jint>(v);
+}
+
+jlong ToJlong(int64 v)
+{
+  return static_cast<jlong>(v);
+}
+jlong ToJlong(float v)
+{
+  return static_cast<jlong>(v);
+}
+jlong ToJlong(double v)
+{
+  return static_cast<jlong>(v);
+}
+jlong ToJlong(tJNIWrappable* v)
+{
+  return (jlong)v;  // Pointer conversion
+}
+jlong ToJlong(const tJNIWrappable& v)
+{
+  return (jlong)&v;  // Pointer conversion
+}
+
+template <typename T>
+jchar ToJchar(T v)
+{
+  return static_cast<jchar>(v);
+}
+
+template <typename T>
+jfloat ToJfloat(T v)
+{
+  return static_cast<jfloat>(v);
+}
+
+template <typename T>
+jdouble ToJdouble(T v)
+{
+  return static_cast<jdouble>(v);
+}
+
+jstring ToJstring(const char* c)
+{
+  jstring js = tJNIHelper::GetEnv()->NewStringUTF(c);
+  return js;
+}
+jstring ToJstring(const finroc::util::tAbstractString& d)
+{
+  return ToJstring(d.GetCString());
+}
+jstring ToJstring(const std::string& d)
+{
+  return ToJstring(d.c_str());
+}
+
+jobject ToJobject(tJNIWrappable* ptr)
+{
+  return ptr->GetJavaWrapper();
+}
+jobject ToJobject(tJNIWrappable& ref)
+{
+  return ref.GetJavaWrapper();
+}
+template<typename T>
+jobject ToJobject(T* ptr)
+{
+  return tCastCache::DynamicCast<tJNIWrappable>(ptr)->GetJavaWrapper();
+}
+template<typename T>
+jobject ToJobject(T& ref)
+{
+  return tCastCache::DynamicCast<tJNIWrappable>(&ref)->GetJavaWrapper();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// conversion methods Java to C++
+///////////////////////////////////////////////////////////////////////////////
+
+int ToInt(jint v)
+{
+  return static_cast<int>(v);
+}
+int ToInt(jlong v)
+{
+  return static_cast<int>(v);
+}
+int ToInt(jbyte v)
+{
+  return static_cast<int>(v);
+}
+int ToInt(jshort v)
+{
+  return static_cast<int>(v);
+}
+
+int16 ToInt16(jint v)
+{
+  return static_cast<int16>(v);
+}
+int16 ToInt16(jlong v)
+{
+  return static_cast<int16>(v);
+}
+int16 ToInt16(jbyte v)
+{
+  return static_cast<int16>(v);
+}
+int16 ToInt16(jshort v)
+{
+  return static_cast<int16>(v);
+}
+
+int8 ToInt8(jint v)
+{
+  return static_cast<int8>(v);
+}
+int8 ToInt8(jlong v)
+{
+  return static_cast<int8>(v);
+}
+int8 ToInt8(jbyte v)
+{
+  return static_cast<int8>(v);
+}
+int8 ToInt8(jshort v)
+{
+  return static_cast<int8>(v);
+}
+
+int64 ToInt64(jint v)
+{
+  return static_cast<int64>(v);
+}
+int64 ToInt64(jlong v)
+{
+  return static_cast<int64>(v);
+}
+int64 ToInt64(jbyte v)
+{
+  return static_cast<int64>(v);
+}
+int64 ToInt64(jshort v)
+{
+  return static_cast<int64>(v);
+}
+
+double ToDouble(jdouble v)
+{
+  return static_cast<jdouble>(v);
+}
+float ToFloat(jfloat v)
+{
+  return static_cast<jfloat>(v);
+}
+
+template <typename T>
+T* StaticCast(jlong ptr)
+{
+  tJNIWrappable* jw = (tJNIWrappable*)ptr;
+  return static_cast<T>(jw);
+}
+
+template <typename T>
+T* DynamicCast(jlong ptr)
+{
+  tJNIWrappable* jw = (tJNIWrappable*)ptr;
+  return tCastCache::DynamicCast<T>(jw);
+}
+
+} // namespace jni
+} // namespace util
+} // namespace finroc
+
+#endif
+
+#endif // RRLIB__FINROC_CORE_UTILS_JNI__TJNICONVERT_H
