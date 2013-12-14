6e98e70f1d21c11daec2ca06296981678905b065
Max Reichardt <mreichardt@gmx.net>
2013-12-14 01:10:16 +0100

    Adapted to moving jni functions to rrlib_jni-java

--- java_native_utility_functions_jni.cpp
+++ java_native_utility_functions_jni.cpp
@@ -71,39 +71,39 @@ namespace jni
 
 extern "C"
 {
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_deleteJNIWrappable(JNIEnv* env, jclass class_, jlong pointer)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_deleteJNIWrappable(JNIEnv* env, jclass class_, jlong pointer)
   {
     rrlib::jni::tJNIWrappable* obj = (rrlib::jni::tJNIWrappable*)pointer;
     obj->SetJavaWrapper(NULL, false); // avoids that Java "destructor" is invoked (again)
     delete obj;
   }
-  JNIEXPORT jlong JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getBufferPointer(JNIEnv* env, jclass class_, jobject buf)
+  JNIEXPORT jlong JNICALL Java_org_rrlib_jni_JNICalls_getBufferPointer(JNIEnv* env, jclass class_, jobject buf)
   {
     return (jlong)env->GetDirectBufferAddress(buf);
   }
-  JNIEXPORT jbyte JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getByte(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jbyte JNICALL Java_org_rrlib_jni_JNICalls_getByte(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jbyte*)ptr);
   }
-  JNIEXPORT jobject JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getCByteBuffer(JNIEnv* env, jclass class_, jlong ptr, jint size)
+  JNIEXPORT jobject JNICALL Java_org_rrlib_jni_JNICalls_getCByteBuffer(JNIEnv* env, jclass class_, jlong ptr, jint size)
   {
     jobject result;
     result = env->NewDirectByteBuffer((void*)ptr, size);
     return result;
   }
-  JNIEXPORT jdouble JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getDouble(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jdouble JNICALL Java_org_rrlib_jni_JNICalls_getDouble(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jdouble*)ptr);
   }
-  JNIEXPORT jfloat JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getFloat(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jfloat JNICALL Java_org_rrlib_jni_JNICalls_getFloat(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jfloat*)ptr);
   }
-  JNIEXPORT jint JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getInt(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jint JNICALL Java_org_rrlib_jni_JNICalls_getInt(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jint*)ptr);
   }
-  JNIEXPORT jlong JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getJavaVM(JNIEnv* env, jclass class_)
+  JNIEXPORT jlong JNICALL Java_org_rrlib_jni_JNICalls_getJavaVM(JNIEnv* env, jclass class_)
   {
     JavaVM* jvm = rrlib::jni::GetJavaVM();
     if (jvm != NULL)
@@ -115,66 +115,66 @@ extern "C"
     rrlib::jni::SetJavaVM(jvm);
     return (jlong)jvm;
   }
-  JNIEXPORT jlong JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getLong(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jlong JNICALL Java_org_rrlib_jni_JNICalls_getLong(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jlong*)ptr);
   }
-  JNIEXPORT jlong JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getPointer(JNIEnv* env, jclass class_, jlong address, jint index)
+  JNIEXPORT jlong JNICALL Java_org_rrlib_jni_JNICalls_getPointer(JNIEnv* env, jclass class_, jlong address, jint index)
   {
     void** array = (void**)address;
     return (jlong)array[index];
   }
-  JNIEXPORT jshort JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_getShort(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jshort JNICALL Java_org_rrlib_jni_JNICalls_getShort(JNIEnv* env, jclass class_, jlong ptr)
   {
     return *((jshort*)ptr);
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_memcpy(JNIEnv* env, jclass class_, jlong dest, jlong src, jint length)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_memcpy(JNIEnv* env, jclass class_, jlong dest, jlong src, jint length)
   {
     memcpy((void*)src, (void*)dest, (int)length);
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setByte(JNIEnv* env, jclass class_, jlong ptr, jbyte val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setByte(JNIEnv* env, jclass class_, jlong ptr, jbyte val)
   {
     *((jbyte*)ptr) = val;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setDouble(JNIEnv* env, jclass class_, jlong ptr, jdouble val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setDouble(JNIEnv* env, jclass class_, jlong ptr, jdouble val)
   {
     *((jdouble*)ptr) = val;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setFloat(JNIEnv* env, jclass class_, jlong ptr, jfloat val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setFloat(JNIEnv* env, jclass class_, jlong ptr, jfloat val)
   {
     *((jfloat*)ptr) = val;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setInt(JNIEnv* env, jclass class_, jlong ptr, jint val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setInt(JNIEnv* env, jclass class_, jlong ptr, jint val)
   {
     *((jint*)ptr) = val;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setJavaObject(JNIEnv* env, jclass class_, jlong pointer, jobject object, jboolean java_responsible)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setJavaObject(JNIEnv* env, jclass class_, jlong pointer, jobject object, jboolean java_responsible)
   {
     rrlib::jni::tJNIWrappable* obj = (rrlib::jni::tJNIWrappable*)pointer;
     obj->SetJavaWrapper(object, !java_responsible);
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setLong(JNIEnv* env, jclass class_, jlong ptr, jlong val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setLong(JNIEnv* env, jclass class_, jlong ptr, jlong val)
   {
     *((jlong*)ptr) = val;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setPointer(JNIEnv* env, jclass class_, jlong arraypointer, jint index, jlong pointer)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setPointer(JNIEnv* env, jclass class_, jlong arraypointer, jint index, jlong pointer)
   {
     void** array = (void**)arraypointer;
     array[index] = (void*)pointer;
   }
-  JNIEXPORT void JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_setShort(JNIEnv* env, jclass class_, jlong ptr, jshort val)
+  JNIEXPORT void JNICALL Java_org_rrlib_jni_JNICalls_setShort(JNIEnv* env, jclass class_, jlong ptr, jshort val)
   {
     *((jshort*)ptr) = val;
   }
-  JNIEXPORT jint JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_sizeOfPointer(JNIEnv* env, jclass class_)
+  JNIEXPORT jint JNICALL Java_org_rrlib_jni_JNICalls_sizeOfPointer(JNIEnv* env, jclass class_)
   {
     return sizeof(void*);
   }
-  JNIEXPORT jint JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_strlen(JNIEnv* env, jclass class_, jlong ptr)
+  JNIEXPORT jint JNICALL Java_org_rrlib_jni_JNICalls_strlen(JNIEnv* env, jclass class_, jlong ptr)
   {
     return strlen((char*)ptr);
   }
-  JNIEXPORT jstring JNICALL Java_org_rrlib_finroc_1core_1utils_jc_jni_JNICalls_toString(JNIEnv* env, jclass class_, jlong pointer)
+  JNIEXPORT jstring JNICALL Java_org_rrlib_jni_JNICalls_toString(JNIEnv* env, jclass class_, jlong pointer)
   {
     jstring result;
     result = env->NewStringUTF((char*)pointer);
